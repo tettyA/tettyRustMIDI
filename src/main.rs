4821c880eb5bf9db543c89c83bac36f64fc57c02
tettyA <78031369+tettyA@users.noreply.github.com>
2025-10-31 00:11:29 +0900

    Change MIDI file path and log track events to console
    Updated MIDI file path and modified output to console.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,49 @@ use std::path::Path;
 mod midifile;
 
 use midifile::MIDIFile;
-
+use midifile::track::midievent;
 fn main() {
-    let midifile: MIDIFile = MIDIFile::load(Path::new(r#"C:\hoge\Fur Elise.mid"#));
+    let midifile: MIDIFile = MIDIFile::load(Path::new(
+        r#"C:\hoge\fuga.mid"#,
+    ));
 
     println!(
         "format:{}\ntrack_count:{}\nresolution:{}",
         midifile.header.format, midifile.header.track_count, midifile.header.resolution
     );
-    //ファイルに出力
-    let mut output_file = std::fs::File::create("output.txt").unwrap();
-    writeln!(
-        output_file,
-        "format:{}\ntrack_count:{}\nresolution:{}",
-        midifile.header.format, midifile.header.track_count, midifile.header.resolution
-    );
     for i in 0..midifile.header.track_count {
-        writeln!(output_file, "---- Track {} ----", i + 1);
+        println!("--- Track {} ---", i);
         for event in &midifile.tracks[i as usize].events {
-            writeln!(output_file, "{}, {}", event.delta_time, event.message);
+            // println!(output_file, "{}, {}", event.delta_time, event.message);
+
+            match &event.message {
+                midievent::Message::Meta(meta_event) => match meta_event {
+                    midievent::MetaEvent::SetTempo(tempo) => {
+                        println!("Tempo Change: {}", tempo);
+                    }
+                    midievent::MetaEvent::Lyrics { len, text } => {
+                        print!("Lyrics: len {}, text: ", len);
+                        for c in text {
+                            print!("{}", *c as char);
+                        }
+                        println!();
+                    }
+                    midievent::MetaEvent::EndOfTrack => {
+                        println!("End of Track");
+                    }
+                    midievent::MetaEvent::KeySignature { sf, mi } => {
+                        println!("Key Signature: sf {}, mi {}", sf, mi);
+                    }
+                    _ => {}
+                },
+                midievent::Message::PitchBentoChange { channel, lsb, msb } => {
+                    println!(
+                        "PitchBentoChange: channel {}, lsb {}, msb {}",
+                        channel, lsb, msb
+                    );
+                }
+                _ => {}
+            }
         }
     }
 }
